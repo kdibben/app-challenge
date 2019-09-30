82efe924e803c0e29e81e2bad51de8ae8b7adc0f
Kelsie Dibben <kmdibben@gmail.com>
2019-09-30 12:20:55 -0500

    Working app

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,8 +1,8 @@
-const items = document.getElementById("items").value 
-const sizeEach = document.getElementById("sizeEach").value
-const result = document.getElementById("result")
-
 function calculateStorage() {
+    const items = document.getElementById("items").value 
+    const sizeEach = document.getElementById("sizeEach").value
+    const result = document.getElementById("result")
+
     const storage = items * sizeEach
 
     result.innerHTML = `The estimated storage size is: ${storage}`
